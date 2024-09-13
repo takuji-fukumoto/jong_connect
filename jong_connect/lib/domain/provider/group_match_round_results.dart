ebd34a8a99d4f2c079affd90d16c0e65899f542a
takuji-fukumoto <fumo8009@gmail.com>
2024-09-13 22:47:44 +0900

    不要コード削除

--- jong_connect/lib/domain/provider/group_match_round_results.dart
+++ jong_connect/lib/domain/provider/group_match_round_results.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
 import 'package:jong_connect/data/group_match_results_repository.dart';
-import 'package:jong_connect/data/group_matches_repository.dart';
-import 'package:jong_connect/domain/model/group_match.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../model/group_match_result.dart';
