5d7e21dcfdb4cdf31c3a02260a9bc2cc0512cc34
takuji-fukumoto <fumo8009@gmail.com>
2024-09-14 21:59:19 +0900

    自身のフレンドIDをコピーできるようにする

--- jong_connect/lib/presentation/pages/home/my_friend_id_section.dart
+++ jong_connect/lib/presentation/pages/home/my_friend_id_section.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jong_connect/domain/provider/current_user.dart';
 import 'package:jong_connect/presentation/common_widgets/async_value_widget.dart';
 import 'package:jong_connect/util/app_sizes.dart';
+
+import '../../../util/constants.dart';
 
 class MyFriendIdSection extends ConsumerWidget {
   const MyFriendIdSection({super.key});
@@ -23,7 +26,21 @@ class MyFriendIdSection extends ConsumerWidget {
             gapH16,
             Text('ここにQRコード表示'),
             gapH16,
-            Text(user.hashedFriendId),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(user.hashedFriendId),
+                gapW16,
+                IconButton(
+                  onPressed: () async {
+                    final data = ClipboardData(text: user.hashedFriendId);
+                    await Clipboard.setData(data);
+                    SnackBarService.showSnackBar(content: 'クリップボードにコピーしました');
+                  },
+                  icon: const Icon(Icons.copy),
+                ),
+              ],
+            )
           ],
         );
       },
