5ca3bc715f2167ce6f10fe65ec358623086bb48a
takuji-fukumoto <fumo8009@gmail.com>
2025-06-23 15:32:32 +0900

    usecase修正

---
+++ jong_connect/lib/usecase/season_use_case.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:jong_connect/data/seasons_repository.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import 'package:jong_connect/domain/model/season.dart';
+
+part 'season_use_case.g.dart';
+
+@riverpod
+SeasonUseCase seasonUseCase(SeasonUseCaseRef ref) => SeasonUseCase(ref);
+
+class SeasonUseCase {
+  final SeasonUseCaseRef _ref;
+
+  SeasonUseCase(this._ref);
+
+  Future<void> createGroupSeason(
+      {required String name,
+      required String description,
+      required int groupId}) async {
+    return await _ref
+        .read(seasonsRepositoryProvider)
+        .create(name, description, groupId);
+  }
+
+  Future<void> updateSeason(Season newSeason) async {
+    await _ref.read(seasonsRepositoryProvider).update(newSeason);
+  }
+
+  Future<void> deleteSeason(int seasonId) async {
+    await _ref.read(seasonsRepositoryProvider).delete(seasonId);
+  }
+}
--- jong_connect/lib/usecase/season_use_case.g.dart
+++ jong_connect/lib/usecase/season_use_case.g.dart
@@ -1,27 +1,25 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'create_season_use_case.dart';
+part of 'season_use_case.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$createSeasonUseCaseHash() =>
-    r'ed0d87dc3c8640895e6b47b3648b13be96728555';
+String _$seasonUseCaseHash() => r'9e8bd1b9b516d0e56b69f2f703bece53111acc55';
 
-/// See also [createSeasonUseCase].
-@ProviderFor(createSeasonUseCase)
-final createSeasonUseCaseProvider =
-    AutoDisposeProvider<CreateSeasonUseCase>.internal(
-  createSeasonUseCase,
-  name: r'createSeasonUseCaseProvider',
+/// See also [seasonUseCase].
+@ProviderFor(seasonUseCase)
+final seasonUseCaseProvider = AutoDisposeProvider<SeasonUseCase>.internal(
+  seasonUseCase,
+  name: r'seasonUseCaseProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$createSeasonUseCaseHash,
+      : _$seasonUseCaseHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
-typedef CreateSeasonUseCaseRef = AutoDisposeProviderRef<CreateSeasonUseCase>;
+typedef SeasonUseCaseRef = AutoDisposeProviderRef<SeasonUseCase>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
