e5723556b44b90f2c78d59577036cea6904a8de0
takuji-fukumoto <fumo8009@gmail.com>
2025-07-15 22:54:49 +0900

    pull to refresh追加

--- jong_connect/lib/presentation/pages/home/home_page.dart
+++ jong_connect/lib/presentation/pages/home/home_page.dart
@@ -1,29 +1,54 @@
+import 'dart:math';
+
+import 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jong_connect/presentation/pages/home/my_profile_section.dart';
 import 'package:jong_connect/presentation/pages/home/recently_match_results_section.dart';
 import 'package:jong_connect/util/app_sizes.dart';
 
-class HomePage extends StatelessWidget {
+import '../../../domain/provider/recently_match_results.dart';
+
+class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
         foregroundColor: Theme.of(context).colorScheme.surface,
         title: const Text('ホーム'),
       ),
-      body: const SingleChildScrollView(
-        padding: paddingV20H16,
-        child: Column(
-          children: [
-            MyProfileSection(),
-            gapH8,
-            RecentlyGroupMatchesSection(),
-          ],
+      body: CustomMaterialIndicator(
+        onRefresh: () => onRefresh(ref),
+        backgroundColor: Colors.white,
+        indicatorBuilder: (context, controller) {
+          return Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: CircularProgressIndicator(
+              color: Colors.redAccent,
+              value: controller.state.isLoading
+                  ? null
+                  : min(controller.value, 1.0),
+            ),
+          );
+        },
+        child: const SingleChildScrollView(
+          padding: paddingV20H16,
+          child: Column(
+            children: [
+              MyProfileSection(),
+              gapH8,
+              RecentlyGroupMatchesSection(),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> onRefresh(WidgetRef ref) async {
+    ref.invalidate(recentlyMatchResultsProvider);
   }
 }
