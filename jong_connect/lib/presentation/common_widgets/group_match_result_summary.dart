c3dceed16106aabf695c02c702dd541bc57878df
takuji-fukumoto <fumo8009@gmail.com>
2025-07-15 22:22:19 +0900

    仮実装

---
+++ jong_connect/lib/presentation/common_widgets/group_match_result_summary.dart
@@ -0,0 +1,150 @@
+import 'dart:collection';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:jong_connect/domain/model/app_user.dart';
+import 'package:jong_connect/domain/model/group_match.dart';
+import 'package:jong_connect/util/app_sizes.dart';
+import '../../../util/routing_path.dart';
+import '../../util/format_date.dart';
+
+class GroupMatchResultSummary extends StatelessWidget {
+  const GroupMatchResultSummary({super.key, required this.match});
+
+  final GroupMatch match;
+
+  @override
+  Widget build(BuildContext context) {
+    if (match.results!.isEmpty) {
+      return const SizedBox();
+    }
+
+    final size = MediaQuery.of(context).size;
+    final startAt = DateFormatter(match.createdAt).formatToYYYYMMDDHHmm();
+    final endAt = match.endAt != null
+        ? DateFormatter(match.endAt!).formatToYYYYMMDDHHmm()
+        : "";
+
+    return GestureDetector(
+      onTap: () => {
+        context.goNamed(
+          RoutingPath.groupMatch,
+          pathParameters: {
+            'groupId': match.groupId.toString(),
+            'groupMatchId': match.id.toString(),
+          },
+        )
+      },
+      child: Container(
+        height: size.height * 0.25,
+        padding: paddingV8H8,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surfaceContainerHigh,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(10),
+          ),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(match.group!.name),
+                Text(match.season?.name ?? ""),
+              ],
+            ),
+            gapH4,
+            Row(
+              children: [
+                Text(startAt),
+                if (match.isFinish) ...[
+                  gapW8,
+                  const Text("-"),
+                  gapW8,
+                  Text(endAt),
+                ]
+              ],
+            ),
+            gapH8,
+            Expanded(
+              child: _GroupMatchResultListView(match: match),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _GroupMatchResultListView extends StatelessWidget {
+  const _GroupMatchResultListView({super.key, required this.match});
+
+  final GroupMatch match;
+  final int rowCount = 2;
+
+  @override
+  Widget build(BuildContext context) {
+    var totalResults = match.totalPointsPerUser;
+    var sortedResults = SplayTreeMap<String, int>.from(
+        totalResults, (a, b) => totalResults[b]!.compareTo(totalResults[a]!));
+    var playerIds = sortedResults.keys.toList();
+    var playerMap = Map.fromIterables(
+        match.joinUsers.map<String>((user) => user.id).toList(),
+        match.joinUsers.map<AppUser>((user) => user).toList());
+
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: [
+          for (var playerId in playerIds)
+            _UserScoreListItem(
+              user: playerMap[playerId]!,
+              score: sortedResults[playerId]!,
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _UserScoreListItem extends StatelessWidget {
+  const _UserScoreListItem(
+      {super.key, required this.user, required this.score});
+
+  final AppUser user;
+  final int score;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width * 0.2,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: CachedNetworkImage(
+              imageUrl: user.avatarUrl,
+              imageBuilder: (context, imageProvider) => CircleAvatar(
+                radius: 40,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: imageProvider,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          gapH4,
+          Text(user.name),
+          gapH8,
+          Text(score.toString()),
+        ],
+      ),
+    );
+  }
+}
--- jong_connect/lib/presentation/pages/home/recently_match_results_section.dart
+++ jong_connect/lib/presentation/pages/home/recently_match_results_section.dart
@@ -1,18 +1,10 @@
-import 'package:auto_size_text/auto_size_text.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:jong_connect/domain/model/group_match.dart';
-import 'package:jong_connect/domain/provider/group_match.dart';
 import 'package:jong_connect/domain/provider/recently_match_results.dart';
 import 'package:jong_connect/presentation/common_widgets/async_value_widget.dart';
-import 'package:jong_connect/util/app_icon_urls.dart';
 import 'package:jong_connect/util/app_sizes.dart';
-import 'package:jong_connect/util/format_date.dart';
 
-import '../../../domain/model/group_round_match_result.dart';
-import '../../common_widgets/user_profile_dialog.dart';
+import '../../common_widgets/group_match_result_summary.dart';
 
 class RecentlyGroupMatchesSection extends ConsumerWidget {
   const RecentlyGroupMatchesSection({super.key});
@@ -63,8 +55,8 @@ class _ResultsList extends ConsumerWidget {
               ),
             ] else ...[
               for (var groupMatch in groupMatches) ...{
-                _GroupMatchListTile(
-                  groupMatch: groupMatch,
+                GroupMatchResultSummary(
+                  match: groupMatch,
                 ),
                 gapH8,
               },
@@ -72,198 +64,6 @@ class _ResultsList extends ConsumerWidget {
           ],
         );
       },
-    );
-  }
-}
-
-class _GroupMatchListTile extends StatelessWidget {
-  const _GroupMatchListTile({super.key, required this.groupMatch});
-
-  final GroupMatch groupMatch;
-
-  @override
-  Widget build(BuildContext context) {
-    if (groupMatch.results!.isEmpty) {
-      return const SizedBox();
-    }
-
-    final startAt = DateFormatter(groupMatch.createdAt).formatToYYYYMMDDHHmm();
-    final endAt = groupMatch.endAt != null
-        ? DateFormatter(groupMatch.endAt!).formatToYYYYMMDDHHmm()
-        : "";
-
-    return Container(
-      padding: paddingV8H8,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surfaceContainerHigh,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(10),
-        ),
-      ),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(groupMatch.group!.name),
-              Text(groupMatch.season?.name ?? ""),
-            ],
-          ),
-          gapH4,
-          Row(
-            children: [
-              Text(startAt),
-              if (endAt.isNotEmpty) ...[
-                gapW8,
-                const Text("-"),
-                gapW8,
-                Text(endAt),
-              ]
-            ],
-          ),
-          gapH8,
-          for (var matchResult in groupMatch.results!) ...[
-            Text(matchResult.matchOrder.toString()),
-            Text(matchResult.userDisplayName.toString()),
-            Text(matchResult.score.toString()),
-            Text(matchResult.rank.toString()),
-            // _ResultListTile(roundResult: roundResult)
-          ]
-        ],
-      ),
-    );
-  }
-}
-
-class _ResultListTile extends StatelessWidget {
-  const _ResultListTile({super.key, required this.roundResult});
-
-  final GroupRoundMatchResult roundResult;
-
-  @override
-  Widget build(BuildContext context) {
-    final dateFormatter = DateFormatter(roundResult.results.first.createdAt);
-
-    return Padding(
-      padding: paddingV8H8,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // TODO: グループ名とシーズンセクション追加
-          // TODO: 対戦日付のセクション修正
-          Text(
-            dateFormatter.formatToYYYYMMDDHHmm(),
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.outline,
-            ),
-          ),
-          gapH4,
-          StaggeredGrid.count(
-            axisDirection: AxisDirection.down,
-            crossAxisCount: 2,
-            mainAxisSpacing: Sizes.p4,
-            crossAxisSpacing: Sizes.p4,
-            children: [
-              for (var result in roundResult.resultsOrderByRank) ...[
-                StaggeredGridTile.count(
-                  crossAxisCellCount: 1,
-                  mainAxisCellCount: 0.4,
-                  child: GestureDetector(
-                    onTap: () async {
-                      await showDialog(
-                        context: context,
-                        builder: (context) => UserProfileDialog(
-                          user: result.user!,
-                        ),
-                      );
-                    },
-                    child: Container(
-                      padding: paddingV8H8,
-                      decoration: BoxDecoration(
-                        color: Theme.of(context)
-                            .colorScheme
-                            .surfaceContainerHighest,
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(15)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: Column(
-                              children: [
-                                if (result.rank == 1)
-                                  Expanded(
-                                    flex: 1,
-                                    child: CachedNetworkImage(
-                                        width: Sizes.p16,
-                                        height: Sizes.p16,
-                                        imageUrl: AppIconUrls.crown),
-                                  )
-                                else
-                                  const Expanded(flex: 1, child: SizedBox()),
-                                Text(
-                                  result.rank.toString(),
-                                  style: const TextStyle(
-                                    fontSize: Sizes.p16,
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 2,
-                                  child: Text(
-                                    result.totalPoints.toString(),
-                                    textAlign: TextAlign.center,
-                                    style: TextStyle(
-                                      fontSize: Sizes.p12,
-                                      color: result.totalPoints > 0
-                                          ? Colors.blueAccent
-                                          : result.totalPoints < 0
-                                              ? Colors.redAccent
-                                              : Theme.of(context)
-                                                  .colorScheme
-                                                  .inverseSurface,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: CircleAvatar(
-                              child: CachedNetworkImage(
-                                  imageUrl: result.user!.avatarUrl),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 4,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                AutoSizeText(
-                                  result.user!.name,
-                                  minFontSize: Sizes.p8,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                gapH8,
-                                AutoSizeText(
-                                  result.score.toString(),
-                                  minFontSize: Sizes.p8,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ],
-          ),
-        ],
-      ),
     );
   }
 }
